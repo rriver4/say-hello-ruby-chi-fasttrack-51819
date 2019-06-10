88ab0cd948d04e979dfc35972873250c3865425b
loyal-recipe-0842 <riveraronirae@gmail.com>
2019-06-10 01:02:54 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,3 +1,8 @@
+def greeting(name)
+  puts "Hello, #{name}"
+end
+
+
 def greeting(name="Ruby programmer", language="Ruby")
   puts "Hello, #{name}. We heard you are a great #{language} programmer."
 end
