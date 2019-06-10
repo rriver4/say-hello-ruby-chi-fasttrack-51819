797ded33a72a5ca167ef9acc8cb5e5e6ebce9607
loyal-recipe-0842 <riveraronirae@gmail.com>
2019-06-10 01:03:34 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,9 +1,9 @@
-def greeting(name)
-  puts "Hello, #{name}"
-end
-
-
 def greeting(name="Ruby programmer", language="Ruby")
   puts "Hello, #{name}. We heard you are a great #{language} programmer."
 end
 greeting("Steven", "Ruby")# Build your say_hello method here
+
+
+def greeting(name)
+  puts "Hello, #{name}"
+end
